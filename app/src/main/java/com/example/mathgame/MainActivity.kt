32f46a9aab13a667787049ecda14e878dee48760
Joao <joao.paulo199916@gmail.com>
2024-01-15 22:07:54 +0000

    15-01-2024 Commit

--- app/src/main/java/com/example/mathgame/MainActivity.kt
+++ app/src/main/java/com/example/mathgame/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.mathgame
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var  adicao : Button
+    lateinit var subtracao : Button
+    lateinit var multi : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adicao = findViewById(R.id.btn_Add)
+        subtracao = findViewById(R.id.btn_Subtracao)
+        multi = findViewById(R.id.btn_Multi)
+
+        adicao.setOnClickListener {
+
+            val intent = Intent(this@MainActivity, GameActivity::class.java)
+            startActivity(intent)
+
+        }
+
     }
 }
